0d01e21a7c8c0389045c50ca1bd1482156caebb7
KlausMikhaelson <satyamsingh5076@gmail.com>
2022-06-29 15:39:59 +0530

    mongo connected and created setedup in env

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+require("dotenv").config()
+const mongoose = require("mongoose")
 const multer = require("multer")
 const app = express()
 
 const upload = multer({ dest: "uploads" })
+
+mongoose.connect(process.env.DATABASE_URL)
 
 app.set("view engine", "ejs")
 
@@ -14,4 +18,4 @@ app.post("/upload", upload.single("file"), (req, res) =>{
     res.send("Done")
 })
 
-app.listen(3000)
+app.listen(process.env.PORT)
